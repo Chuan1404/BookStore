1d65e3a351b8b10999f2d1bd42d5384410384b04
Alvid Nguyen <d.fed22@gsoft01.tk>
2022-11-13 13:05:09 +0700

    setup dataabase

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 from app import app, db
-from . import User,Category_book, Receipt, Received_note, Rule
+from . import User, Category_book, Receipt, Received_note, Rule
 
 with app.app_context():
     db.create_all()
