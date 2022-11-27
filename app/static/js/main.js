0a2a247c0eca16c9ff6b3019c1310a65d67bb46f
PhuNguyen8902 <89370742+PhuNguyen8902@users.noreply.github.com>
2022-11-27 21:30:06 +0700

    train

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,14 +1,13 @@
-import { accordion, activeCardbox, addClass, toggleClass } from './modules.js'
+import { accordion, activeCardbox, addClass, toggleClass } from "./modules.js";
 
-let userCard = document.querySelector('.header__card .user')
-let headerBox = document.querySelector('.header__box')
+let userCard = document.querySelector(".header__card .user");
+let headerBox = document.querySelector(".header__box");
 // execute when html loaded
-window.addEventListener('load', () => {
-    activeCardbox()
-    accordion()
+window.addEventListener("load", () => {
+  activeCardbox();
+  accordion();
 
-    userCard.addEventListener('click', e => {
-        toggleClass(headerBox, 'active')
-    })
-})
-
+  userCard.addEventListener("click", (e) => {
+    toggleClass(headerBox, "active");
+  });
+});
