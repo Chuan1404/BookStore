10cc1ec77989048c61f5237596de883cd2800813
An Chu <chuan14402@gmail.com>
2022-12-02 18:28:31 +0700

    import book

--- app/static/js/importBook.js
+++ app/static/js/importBook.js
@@ -1,17 +1,19 @@
 // execute when html loaded
 window.addEventListener("load", () => {
   let dateInput = document.querySelector("#date");
-  setInputValue(dateInput)
+  setInputValue(dateInput);
 
   dateInput?.addEventListener("change", (e) => {
-    window.location.search = `?current_date=${e.target.value}`
+    window.location.search = `?current_date=${e.target.value}`;
   });
-
 });
 
 function setInputValue(input) {
-  let search = window.location.search
+  let search = window.location.search;
   if (search) {
-    input.value = search.split('=')[1]
+    input.value = search.split("=")[1];
+  } else {
+    let date = new Date();
+    input.value = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate().toString().padStart(2, '0')}`
   }
 }
